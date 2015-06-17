7be9673faad379a8d8db30b52b83cc20c59570ac
Andrew Boik <dboik@cisco.com>
2015-06-17 10:26:33 -0400

    Make ping test and ssh-keyscan idempotent

--- manifests/plugins/ml2/cisco/nexus_creds.pp
+++ manifests/plugins/ml2/cisco/nexus_creds.pp
@@ -33,14 +33,17 @@ define neutron::plugins::ml2::cisco::nexus_creds(
     }
   }
 
+  $check_known_hosts = "/bin/cat /var/lib/neutron/.ssh/known_hosts | /bin/grep ${ip_address}"
+
   # Test to make sure switch is reachable before ssh-keyscan
   exec {"ping_test_${name}":
-    command => "/usr/bin/ping -c 5 ${ip_address}",
+    unless  => $check_known_hosts,
+    command => "/usr/bin/ping -c 1 ${ip_address}",
     user    => 'neutron'
   }
 
   exec {"nexus_creds_${name}":
-    unless  => "/bin/cat /var/lib/neutron/.ssh/known_hosts | /bin/grep ${username}",
+    unless  => $check_known_hosts,
     command => "/usr/bin/ssh-keyscan -t rsa ${ip_address} >> /var/lib/neutron/.ssh/known_hosts",
     user    => 'neutron',
     require => [Exec["ping_test_${name}"], File['/var/lib/neutron/.ssh']]
